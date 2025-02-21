ac2513183481ee145200b2dc6e8188ed96b1fa7a
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-21 12:51:24 +0100

    commit 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import SideBar from "./components/SideBar";
+import MusicSection from "./components/MusicSection";
+import MusicPlayer from "./components/MusicPlayer";
+import SearchResults from "./components/SearchResults";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <SideBar />
+
+        <main className="col-12 col-md-9 offset-md-3 mainPage">
+          <SearchResults />
+
+          <MusicSection />
+          <MusicSection />
+          <MusicSection />
+        </main>
+      </div>
+
+      <MusicPlayer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,49 @@
-function sideBar {
-    return (
+const SideBar = () => {
+  return (
+    <aside className="col col-2">
+      <nav className="navbar navbar-expand-md fixed-left justify-content-between" id="sidebar">
+        <div className="container flex-column align-items-start">
+          <a className="navbar-brand" href="#">
+            <img src="" alt="Spotify Logo" width="131" height="40" />
+          </a>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <ul>
+                <li>
+                  <a className="nav-item nav-link d-flex align-items-center" href="#">
+                    <i className="bi bi-house-door-fill"></i>&nbsp; Home
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-item nav-link d-flex align-items-center" href="#">
+                    <i className="bi bi-book-fill"></i>&nbsp; Your Library
+                  </a>
+                </li>
+                <li>
+                  <div className="input-group mt-3">
+                    <input type="text" className="form-control" placeholder="Search" />
+                    <div className="input-group-append">
+                      <button className="btn btn-outline-secondary btn-sm h-100">GO</button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="nav-btn">
+          <button className="btn signup-btn" type="button">
+            Sign Up
+          </button>
+          <button className="btn login-btn" type="button">
+            Login
+          </button>
+          <br />
+          <a href="#">Cookie Policy</a> | <a href="#"> Privacy</a>
+        </div>
+      </nav>
+    </aside>
+  );
+};
 
-    )
-}
-
-export default sideBar
+export default SideBar;
