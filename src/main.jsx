fa070c50228023309cd9080a44ee080a782eacde
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-21 12:03:07 +0100

    commit 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./redux/store/store";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
